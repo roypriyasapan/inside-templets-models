fa21f4032ff53b56a24d1d0288fe17014b63ca3d
Priya <Priyasapanroy@gmail.com>
2024-07-03 18:29:08 +0530

    second commit

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
+from .models import Student
+
 
 # Create your views here.
 def register(request):
@@ -9,10 +11,13 @@ def registerdata(request):
     # return render(request, 'register.html')
     print(request.method)
     print(request.POST)
-    name=request.POST.get('fname')
-    email=request.POST.get('email')
-    contact=request.POST.get('contactno')
-    print(name,email,contact)
+    var1=request.POST.get('fname')
+    var2=request.POST.get('email')
+    var3=request.POST.get('contactno')
+    # print(name,email,contact)
+    Student.objects.create(Name=var1,Email=var2,Contact=var3)
+
+    print ("DATA SAVE SUCCESSFULLY...!!!!")
 
 
 
